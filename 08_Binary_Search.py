0a5dbfeb96d14bf67b8aaedaff3b1041fc5c282b
ahmedavid <ahmedavid@gmail.com>
2020-04-09 11:48:04 +0400

    08_Binary_Search

---
+++ 08_Binary_Search.py
@@ -0,0 +1,31 @@
+# # O(logn) time | O(logn) space
+# def binarySearch(array, target):
+#     return binarySearchHelper(array, target, 0, len(array) - 1)
+
+# def binarySearchHelper(array, target, left, right):
+#     if left > right:
+#         return -1
+#     middle = (left + right) // 2
+#     potentialMatch = array(middle)
+#     if target == potentialMatch:
+#         return middle
+#     elif target < potentialMatch:
+#         return binarySearchHelper(array, target, left, middle -1)
+#     else:
+#         return binarySearchHelper(array, target, middle + 1, right)
+
+# O(logn) time | O(1) space
+def binarySearch(array, target):
+    return binarySearchHelper(array, target, 0, len(array) - 1)
+
+def binarySearchHelper(array, target, left, right):
+    while left <= right:
+        middle = (left + right) // 2
+        potentialMatch = array(middle)
+        if target == potentialMatch:
+            return middle
+        elif target < potentialMatch:
+            right = middle - 1
+        else:
+            left = middle + 1
+    return -1
