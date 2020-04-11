d2e3efc7d0951f0d7dabd1119de67dcb0c732539
ahmedavid <ahmedavid@gmail.com>
2020-04-11 11:19:26 +0400

    20.BST_Traversal

---
+++ 20_BST_Traversal.py
@@ -0,0 +1,20 @@
+def inOrderTraverse(tree, array):
+    if tree is not None:
+        inOrderTraverse(tree.left, array)
+        array.append(tree.value)
+        inOrderTraverse(tree.right, array)
+    return array
+
+def preOrderTraverse(tree, array):
+    if tree is not None:
+        array.append(tree.value)
+        inOrderTraverse(tree.left, array)
+        inOrderTraverse(tree.right, array)
+    return array
+
+def postOrderTraverse(tree, array):
+    if tree is not None:
+        inOrderTraverse(tree.left, array)
+        inOrderTraverse(tree.right, array)
+        array.append(tree.value)
+    return array
